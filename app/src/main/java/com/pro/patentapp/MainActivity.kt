3317e2ca466e0e47ce42f83bc587abbec2abfdce
altynai2002 <altynay.bakytbekova.02@mail.ru>
2022-03-26 12:39:56 +0600

    first commit

---
+++ app/src/main/java/com/pro/patentapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.pro.patentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        private lateinit var binding: ActivityMainBinding
+
+        private val preferences get() = Injector.preferences
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+//        initFragment()
+
+            if (preferences.getEmail("EMAIL") == ""){
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, RegistFragment())
+                    .commit()
+            } else {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, LoginFragment())
+                    .commit()
+            }
+    }
+}
